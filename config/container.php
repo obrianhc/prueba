3a2ab3333d4022ae4002112627bcedfb550236f6
Cristopher Hernandez <obrian.hc@gmail.com>
2018-09-13 16:05:41 -0600

    Inicializando el repositorio

---
+++ config/container.php
@@ -0,0 +1,102 @@
+<?php
+
+use Slim\Container;
+use Monolog\Handler\RotatingFileHandler;
+use Monolog\Logger;
+use Psr\Log\LoggerInterface;
+use Illuminate\Database\Connectors\ConnectionFactory;
+use Illuminate\Database\Connection;
+
+/** @var \Slim\App $app */
+$container = $app->getContainer();
+
+// Activating routes in a subfolder
+$container['environment'] = function () {
+    $scriptName = $_SERVER['SCRIPT_NAME'];
+    $_SERVER['SCRIPT_NAME'] = dirname(dirname($scriptName)) . '/' . basename($scriptName);
+    return new Slim\Http\Environment($_SERVER);
+};
+
+$container['view'] = function (Container $container) {
+    $settings = $container->get('settings');
+    $viewPath = $settings['twig']['path'];
+
+    $twig = new \Slim\Views\Twig($viewPath, [
+        'cache' => $settings['twig']['cache_enabled'] ? $settings['twig']['cache_path'] : false
+    ]);
+
+    /** @var Twig_Loader_Filesystem $loader */
+    $loader = $twig->getLoader();
+    $loader->addPath($settings['public'], 'public');
+
+    // Instantiate and add Slim specific extension
+    $router = $container->get('router');
+    $uri = \Slim\Http\Uri::createFromEnvironment($container->get('environment'));
+    $twig->addExtension(new \Slim\Views\TwigExtension($router, $uri));
+
+    return $twig;
+};
+
+// Activando el log
+$container['logger'] = function (Container $container) {
+    $settings = $container->get('settings');
+    $logger = new Logger($settings['logger']['name']);
+    
+    $level = $settings['logger']['level'];
+    if (!isset($level)) {
+        $level = Logger::ERROR;
+    }
+    
+    $logFile = $settings['logger']['file'];
+    $handler = new RotatingFileHandler($logFile, 0, $level, true, 0775);
+    $logger->pushHandler($handler);
+    
+    return $logger;
+};
+
+//conexion a la base de datos
+$container['pdo'] = function (Container $container) {
+    $settings = $container->get('settings');
+    
+    $host = $settings['db']['host'];
+    $dbname = $settings['db']['database'];
+    $username = $settings['db']['username'];
+    $password = $settings['db']['password'];
+    $charset = $settings['db']['charset'];
+    $collate = $settings['db']['collation'];
+    
+    $dsn = "mysql:host=$host;dbname=$dbname;charset=$charset";
+    
+    $options = [
+        PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+        PDO::ATTR_PERSISTENT => false,
+        PDO::ATTR_EMULATE_PREPARES => false,
+        PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
+        PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES $charset COLLATE $collate"
+    ];
+
+    return new PDO($dsn, $username, $password, $options);
+};
+
+// Configurando la ejecucion de querys
+$container['db'] = function (Container $container) {
+    $settings = $container->get('settings');
+    
+    $config = [
+        'driver' => 'mysql',
+        'host' => $settings['db']['host'],
+        'database' => $settings['db']['database'],
+        'username' => $settings['db']['username'],
+        'password' => $settings['db']['password'],
+        'charset'  => $settings['db']['charset'],
+        'collation' => $settings['db']['collation'],
+    ];
+    
+    $factory = new ConnectionFactory(new \Illuminate\Container\Container());
+    
+    return $factory->make($config);
+};
+
+$container['pdo'] = function (Container $container) {
+    return $container->get('db')->getPdo();
+};
---
+++ config/routes.php
@@ -0,0 +1,55 @@
+<?php
+
+use Slim\Http\Request;
+use Slim\Http\Response;
+use Psr\Log\LoggerInterface;
+use Slim\Container;
+
+$app->get('/', function (Request $request, Response $response) {
+    $response->getBody()->write("It works! This is the default welcome page.");
+
+    return $response;
+})->setName('root');
+
+$app->get('/hello/{name}', function (Request $request, Response $response) {
+    $name = $request->getAttribute('name');
+    $response->getBody()->write("Hello, $name");
+
+    return $response;
+});
+
+$app->get('/time', function (Request $request, Response $response) {
+    $viewData = [
+        'now' => date('Y-m-d H:i:s')
+    ];
+
+    return $this->get('view')->render($response, 'time.twig', $viewData);
+});
+
+$app->get('/logger-test', function (Request $request, Response $response) {
+    /** @var Container $this */
+    /** @var LoggerInterface $logger */
+
+    $logger = $this->get('logger');
+    $logger->error('My error message!');
+
+    $response->getBody()->write("Success");
+
+    return $response;
+});
+
+// Generando una consulta
+use Illuminate\Database\Connection;
+
+$app->get('/databases', function (Request $request, Response $response) {
+    /** @var Container $this */
+    /** @var Connection $db */
+    
+    $db = $this->get('db');
+
+    // fetch all rows as collection
+    $rows = $db->table('information_schema.schemata')->get();
+
+    // return a json response
+    return $response->withJson($rows);
+});
